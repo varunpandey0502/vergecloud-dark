a9884ef55064d08ba7ea7b388ac1dbc006c42b14
varun <varunpandey0502@gmail.com>
2025-01-06 04:28:40 +0530

    fix: fix vercel issue with lottie files

--- components/subscription-savings.tsx
+++ components/subscription-savings.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { Button } from "./ui/button";
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 import checkAnimation from "@/lottie/check.json";
 
 export default function SubscriptionSavings() {
