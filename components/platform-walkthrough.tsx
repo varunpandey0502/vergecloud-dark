a513a4ebe12e31c540713cf042d7fdb9b7b04016
varun <varunpandey0502@gmail.com>
2025-01-06 03:26:23 +0530

    feat: implement platfrom walkthrough

--- components/platform-walkthrough.tsx
+++ components/platform-walkthrough.tsx
@@ -11,8 +11,14 @@ import {
 
 const slides = [
   {
-    image: "/images/walkthrough/dashboard.png",
+    image: "/images/walkthrough/feature-1.png",
     title: "Seamless Setup, Instant Results",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Mauris ullamcorper malesu ada tellus risus posuere.",
+  },
+  {
+    image: "/images/walkthrough/feature-1.png",
+    title: "Seamless Setup",
     description:
       "Lorem ipsum dolor sit amet consectetur. Mauris ullamcorper malesu ada tellus risus posuere.",
   },
@@ -21,9 +27,16 @@ const slides = [
 
 export default function PlatformWalkthrough() {
   return (
-    <section className="bg-[#030303] py-24 overflow-hidden">
-      <div className="container px-6">
-        <div className="grid lg:grid-cols-2 gap-12 items-center">
+    <section className="relative bg-[#030303] py-24 overflow-hidden">
+      {/* Grid Background */}
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: 'url("/images/walkthrough/grid.png")' }}
+      />
+
+      {/* Content */}
+      <div className="container px-6 relative z-10">
+        <div className="grid lg:grid-cols-2 gap-12 items-start">
           <div className="space-y-6">
             <span className="text-white/60">Platform Walkthrough</span>
             <h2 className="text-3xl md:text-5xl font-bold text-white">
@@ -35,21 +48,18 @@ export default function PlatformWalkthrough() {
             </Button>
           </div>
 
-          <div className="relative">
-            <div className="absolute inset-0 bg-gradient-to-r from-[#030303] to-transparent z-10 w-[20%] left-0" />
-            <div className="absolute inset-0 bg-gradient-to-l from-[#030303] to-transparent z-10 w-[20%] right-0" />
-
+          <div>
             <Carousel
               opts={{
                 align: "start",
                 loop: true,
               }}
-              className="w-full"
+              className="w-full flex flex-col gap-6"
             >
               <CarouselContent>
                 {slides.map((slide, index) => (
                   <CarouselItem key={index} className="md:basis-1/1">
-                    <div className="relative rounded-2xl overflow-hidden">
+                    <div className="relative overflow-hidden">
                       <img
                         src={slide.image}
                         alt="Platform screenshot"
@@ -59,9 +69,10 @@ export default function PlatformWalkthrough() {
                   </CarouselItem>
                 ))}
               </CarouselContent>
-              <div className="absolute bottom-4 left-4 z-20 flex gap-2">
-                <CarouselPrevious className="relative h-8 w-8" />
-                <CarouselNext className="relative h-8 w-8" />
+
+              <div className="flex justify-between">
+                <CarouselPrevious className="static h-8 w-8 text-white" />
+                <CarouselNext className="static h-8 w-8 text-white" />
               </div>
             </Carousel>
           </div>
